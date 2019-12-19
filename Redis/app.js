e209f3c64b86ae18b3a664192345f3f842c3b783
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 19:27:15 -0800

    installed middleware

--- Redis/app.js
+++ Redis/app.js
@@ -12,8 +12,28 @@ const client = redis.createClient();
 const app = express();
 const router = express.Router();
 
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'redis_demo',
+    debug    :  false
+});
 
+app.set('views', path.join(__dirname,'../','views'));
+app.engine('html', require('ejs').renderFile);
 
-app.listin(3000, () => {
+app.use(session({
+    secret: 'ssshhhhh',
+    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl :  260}),
+    saveUninitialized: false,
+    resave: false
+}));
+app.use(cookieParser("secretSign#143_!223"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
