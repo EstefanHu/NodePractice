047e230e967c448f5e0f55445f28252ece9c894b
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 20:06:06 -0800

    installed middleweat

--- Session/app.js
+++ Session/app.js
@@ -7,3 +7,20 @@ const redisStore = require('connect-redis')(session);
 const client  = redis.createClient();
 const router = express.Router();
 const app = express();
+
+app.use(session({
+    secret: 'ssshhhh',
+    store: new redisStore({host: 'localhost', port: 6379, client: client, ttl: 260}),
+    saveUninitialized: false,
+    resave: false
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+
+app.use('/', router);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App started on port ${process.env.PORT || 3000}`);
+});
