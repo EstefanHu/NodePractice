d64575807e2c49c314f3ef75c3647370c29e2692
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 20:30:51 -0800

    Improved spacing

--- Redis/app.js
+++ Redis/app.js
@@ -36,56 +36,57 @@ app.use(bodyParser.json());
 
 function handle_database(req,type,callback) {
   	async.waterfall([
-	function(callback) {
-		pool.getConnection(function(err,connection){
-			if(err) {
-			callback(true);
-			} else {
-			callback(null,connection);
-			}
-		});
-	},
-	function(connection,callback) {
-		var SQLquery;
-		switch(type) {
-			case "login" :
-				SQLquery = "SELECT * from user_login WHERE user_email='"+req.body.user_email+"' AND `user_password`='"+req.body.user_password+"'";
-				break;
-			case "checkEmail" :
-				SQLquery = "SELECT * from user_login WHERE user_email='"+req.body.user_email+"'";
-				break;
-			case "register" :
-				SQLquery = "INSERT into user_login(user_email,user_password,user_name) VALUES ('"+req.body.user_email+"','"+req.body.user_password+"','"+req.body.user_name+"')";
-				break;
-			case "addStatus" :
-				SQLquery = "INSERT into user_status(user_id,user_status) VALUES ("+req.session.key["user_id"]+",'"+req.body.status+"')";
-				break;
-			case "getStatus" :
-				SQLquery = "SELECT * FROM user_status WHERE user_id="+req.session.key["user_id"];
-				break;
-			default :
-				break;
-		}
-		callback(null,connection,SQLquery);
-	},
-	function(connection,SQLquery,callback) {
-		connection.query(SQLquery,function(err,rows){
-			connection.release();
-		if(!err) {
-			if(type === "login") {
-				callback(rows.length === 0 ? false : rows[0]);
-			} else if(type === "getStatus") {
-						callback(rows.length === 0 ? false : rows);
-					} else if(type === "checkEmail") {
-						callback(rows.length === 0 ? false : true);
-					} else {
-						callback(false);
-					}
-			} else {
+		function(callback) {
+			pool.getConnection(function(err,connection){
+				if(err) {
 				callback(true);
+				} else {
+				callback(null,connection);
+				}
+			});
+		},
+		function(connection,callback) {
+			var SQLquery;
+			switch(type) {
+				case "login" :
+					SQLquery = "SELECT * from user_login WHERE user_email='"+req.body.user_email+"' AND `user_password`='"+req.body.user_password+"'";
+					break;
+				case "checkEmail" :
+					SQLquery = "SELECT * from user_login WHERE user_email='"+req.body.user_email+"'";
+					break;
+				case "register" :
+					SQLquery = "INSERT into user_login(user_email,user_password,user_name) VALUES ('"+req.body.user_email+"','"+req.body.user_password+"','"+req.body.user_name+"')";
+					break;
+				case "addStatus" :
+					SQLquery = "INSERT into user_status(user_id,user_status) VALUES ("+req.session.key["user_id"]+",'"+req.body.status+"')";
+					break;
+				case "getStatus" :
+					SQLquery = "SELECT * FROM user_status WHERE user_id="+req.session.key["user_id"];
+					break;
+				default :
+					break;
 			}
-		});
-	}],
+			callback(null,connection,SQLquery);
+		},
+		function(connection,SQLquery,callback) {
+			connection.query(SQLquery,function(err,rows){
+				connection.release();
+			if(!err) {
+				if(type === "login") {
+					callback(rows.length === 0 ? false : rows[0]);
+				} else if(type === "getStatus") {
+							callback(rows.length === 0 ? false : rows);
+						} else if(type === "checkEmail") {
+							callback(rows.length === 0 ? false : true);
+						} else {
+							callback(false);
+						}
+				} else {
+					callback(true);
+				}
+			});
+		}
+	],
 	function(result){
 		if(typeof(result) === "boolean" && result === true) {
 			callback(null);
--- Session/app.js
+++ Session/app.js
@@ -19,6 +19,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
 
+router.get('/',(req,res) => {
+    let sess = req.session;
+    if(sess.email) {
+        return res.redirect('/admin');
+    }
+    res.sendFile('index.html');
+});
+
+router.post('/login',(req,res) => { 
+    req.session.email = req.body.email;
+    res.end('done');
+});
+
+router.get('/admin',(req,res) => {
+    if(req.session.email) {
+        res.write(`<h1>Hello ${req.session.email} </h1><br>`);
+        res.end('<a href='+'/logout'+'>Logout</a>');
+    }
+    else {
+        res.write('<h1>Please login first.</h1>');
+        res.end('<a href='+'/'+'>Login</a>');
+    }
+});
+
+router.get('/logout',(req,res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        res.redirect('/');
+    });
+
+});
+
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000, () => {
