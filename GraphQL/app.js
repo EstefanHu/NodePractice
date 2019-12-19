cb3d02dbef54ec1cac1442b73f3d3f1934cc178d
Estefan Hu <estefanhu074@gmail.com>
2019-12-19 13:09:13 -0800

    Added models

--- GraphQL/app.js
+++ GraphQL/app.js
@@ -19,7 +19,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
-  });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
