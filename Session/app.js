4f055028a4376ae4d19e80c9c1a98b231a0ca176
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 20:02:13 -0800

    Initialized

---
+++ Session/app.js
@@ -0,0 +1,9 @@
+'use strict';
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const redis = require('redis');
+const redisStore = require('connect-redis')(session);
+const client  = redis.createClient();
+const router = express.Router();
+const app = express();
