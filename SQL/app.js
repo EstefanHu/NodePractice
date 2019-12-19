0b4d5c1c83bc5c9dee4e03a095645adb059adccb
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 18:56:16 -0800

    added tanble

--- SQL/app.js
+++ SQL/app.js
@@ -36,6 +36,15 @@ app.get('/deletedb', (req, res) => {
     });
 });
 
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts trable created...');
+    });
+});
+
 app.listen('3000', () => {
     console.log('Server started on port 3000')
 });
