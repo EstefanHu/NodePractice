fa44be902f715cd01e22813929705035298240b0
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 19:05:59 -0800

    Added update

--- SQL/app.js
+++ SQL/app.js
@@ -83,6 +83,18 @@ app.get('/getpost/:id', (req, res) => {
     });
 });
 
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'updated';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post updated');
+    });
+});
+
+
+
 app.listen('3000', () => {
     console.log('Server started on port 3000')
 });
