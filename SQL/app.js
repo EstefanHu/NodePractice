ef8b38206c4834ae0b3a1ecbfaadd1a468dbab32
Estefan Hu <estefanhu074@gmail.com>
2019-12-18 18:30:59 -0800

    esteablished create/delete route

--- SQL/app.js
+++ SQL/app.js
@@ -1,8 +1,39 @@
 'use strict';
 const express = require('express');
 const mysql = require('mysql');
-
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('MySQL connected...');
+});
+
+// create db
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...')
+    });
+});
+
+app.get('/deletedb', (req, res) => {
+    let sql = 'DROP TABLE nodemysql';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database deleted!');
+    })
+})
 
 app.listen('3000', () => {
     console.log('Server started on port 3000')
