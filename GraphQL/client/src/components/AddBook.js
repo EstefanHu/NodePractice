fcb20cbfa1d2bb4ae7fe2fa39616719eee74de67
Estefan Hu <estefanhu074@gmail.com>
2019-12-19 16:10:57 -0800

    Added form submittion

--- GraphQL/client/src/components/AddBook.js
+++ GraphQL/client/src/components/AddBook.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
+import {flowRight as compose} from 'lodash';
 
-import {getAuthorsQuery} from '../queries/queries';
+import {
+    getAuthorsQuery,
+    addBookMutation
+} from '../queries/queries';
 
 
 class AddBook extends Component {
@@ -14,7 +18,7 @@ class AddBook extends Component {
         }
     }
     displayAuthors() {
-        let data = this.props.data;
+        let data = this.props.getAuthorsQuery;
         if(data.loading) {
             return(<option disabled>Loading Authors</option>);
         } else {
@@ -25,7 +29,13 @@ class AddBook extends Component {
     }
     submitForm(e) {
         e.preventDefault();
-        console.log(this.state);
+        this.props.addBookMutation({
+            variables: {
+                name: this.state.name,
+                genre: this.state.genre,
+                authorId: this.state.authorId
+            }
+        });
     }
 	render() {
 		return (
@@ -51,4 +61,7 @@ class AddBook extends Component {
 	}
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, {name: "getAuthorsQuery"}),
+    graphql(addBookMutation, {name: "addBookMutation"})
+)(AddBook);
