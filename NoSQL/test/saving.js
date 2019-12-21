3a4046c48e77106ceac66c3f72a96320148564f1
Estefan Hu <estefanhu074@gmail.com>
2019-12-21 03:27:48 -0800

    completed save testing

--- NoSQL/test/saving.js
+++ NoSQL/test/saving.js
@@ -1,7 +1,17 @@
 const assert = require('assert');
+const User = require('../models/user');
 
 describe('Saving records', () => {
-    it('Saving a record to the database', () => {
-        let user = 
+    it('Saving a record to the database', done => {
+        let user = new User({
+            firstName: 'Estefan',
+            lastName: 'Hu'
+        });
+
+        user.save().then(() => {
+            assert(user.isNew === false);
+        });
+
+        done();
     });
 });
