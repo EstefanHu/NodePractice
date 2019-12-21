a542d0ff37ea623f2485e07ef80900efed2a4636
Estefan Hu <estefanhu074@gmail.com>
2019-12-21 03:34:31 -0800

    added test priority

---
+++ NoSQL/test/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// ES6 promise
+mongoose.Promise = global.Promise;
+
+// Connect to db before test run
+before(done => {
+    // Connects to db creating if doesnt exist
+    const db = 'testaroo';
+    mongoose.connect(`mongodb://localhost/${db}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+    mongoose.connection.once('open', () => {
+        console.log(`connected has been established to ${db}`);
+        done();
+    }).on('err', (err) => {
+        console.log('Connection Error: ' + err);
+    });
+});
+
--- NoSQL/test/demo.js
+++ NoSQL/test/demo.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 
 describe('some demo tests', () => {
-    it('adds two numbers together', () => {
+    it('adds two numbers together', done => {
         assert(2 + 3 === 5);
+        done();
     });
 });
