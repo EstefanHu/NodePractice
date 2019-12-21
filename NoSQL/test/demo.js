e49ea470cdef4638ce84f64912c52bcbce41a412
Estefan Hu <estefanhu074@gmail.com>
2019-12-21 03:24:37 -0800

    added models

--- NoSQL/test/demo.js
+++ NoSQL/test/demo.js
@@ -1,4 +1,4 @@
-const asser = require('assert');
+const assert = require('assert');
 
 describe('some demo tests', () => {
     it('adds two numbers together', () => {
---
+++ NoSQL/test/saving.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Saving records', () => {
+    it('Saving a record to the database', () => {
+        let user = 
+    });
+});
