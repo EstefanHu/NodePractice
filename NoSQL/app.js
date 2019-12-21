452cdc4e18eb089c64a37343e9383b02b7e9e45d
Estefan Hu <estefanhu074@gmail.com>
2019-12-21 02:52:32 -0800

    connected to db

--- NoSQL/app.js
+++ NoSQL/app.js
@@ -4,6 +4,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Connects to db creating if doesnt exist
+const db = 'testaroo';
+mongoose.connect(`mongodb://localhost/${db}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+mongoose.connection.once('open', () => {
+    console.log(`connected has been established to ${db}`);
+}).on('err', (err) => {
+    console.log('Connection Error: ' + err);
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
