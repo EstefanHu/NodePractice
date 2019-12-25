22411212fd81064e217a2f7036bd7c4472652bd2
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 15:00:55 -0800

    working with state

--- ReactToDo/client/src/App.js
+++ ReactToDo/client/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Todos from './components/Todos';
+
 function App() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Take out the dog',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Take out the hitman',
+        completed: false
+      }
+    ]
+  }
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Todos todos={this.state.todos} />
     </div>
   );
 }
---
+++ ReactToDo/client/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Todos</h2>
+      </div>
+    )
+  }
+}
+
+export default Todos
