39c7c16a3b9f35074e9cf61edf84766fc0cee54e
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 15:41:06 -0800

    debugged imports

--- ReactToDo/client/src/App.js
+++ ReactToDo/client/src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// import Todos from './components/Todos';
+import Todos from './components/Todos';
 
 class App extends Component {
-  // state = {
-  //   todos: [
-  //     {
-  //       id: 1,
-  //       title: 'Take out the trash',
-  //       completed: false
-  //     },
-  //     {
-  //       id: 2,
-  //       title: 'Take out the dog',
-  //       completed: true
-  //     },
-  //     {
-  //       id: 3,
-  //       title: 'Take out the hitman',
-  //       completed: false
-  //     }
-  //   ]
-  // }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Take out the dog',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Take out the hitman',
+        completed: false
+      }
+    ]
+  }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
-        {/* <Todos todos={this.state.todos} /> */}
+        <h1>Todos</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
--- ReactToDo/client/src/components/TodoItem.js
+++ ReactToDo/client/src/components/TodoItem.js
@@ -1,7 +1,7 @@
-import React, {Componet} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-export class TodoItem extends Componet {
+export class TodoItem extends Component {
   getStyle = () => {
     return {
       background: '#f4f4f4',
