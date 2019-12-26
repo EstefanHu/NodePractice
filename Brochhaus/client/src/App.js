f5e0c419376843951053cb47b8955d0bd3775d78
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 18:38:21 -0800

    added dependencies

--- Brochhaus/client/src/App.js
+++ Brochhaus/client/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import HomeView from './views/HomeView';
+import CreateView from './views/CreateView';
+import DetailView from './views/DetailView';
+import {ApolloClient, ApolloProvider, createBatchingNetworkInterface} from 'react-apollo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App" />
+  }
 }
 
 export default App;
