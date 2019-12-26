7139a28631a4cd5de06bfa43c9a920f81905dd03
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 18:34:09 -0800

    Set up skeletal routes

--- Brochhaus/server/app.js
+++ Brochhaus/server/app.js
@@ -3,6 +3,17 @@ const express = require('express');
 
 const app = express();
 
+app.get('/many', (req, res) => {
+
+})
+
+app.get('/one', (req, res) => {
+
+});
+
+app.post('/create', (req, res) => {
+  
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
