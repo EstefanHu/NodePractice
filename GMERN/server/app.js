17eef0ce1d410ca2715aed9a8bf219195a997b84
Estefan Hu <estefanhu074@gmail.com>
2019-12-26 00:47:22 -0800

    migrated to apollo-server

--- GMERN/server/app.js
+++ GMERN/server/app.js
@@ -1,8 +1,5 @@
 'use strict';
-const express = require('express');
-const app = express();
-const { buildSchema } = require('graphql');
-const graphqlHTTP = require('express-graphql');
+const {ApolloServer, gql} = require('ApolloServer');
 const crypto = require('crypto');
 
 const db = {
@@ -28,7 +25,7 @@ class User {
   }
 }
 
-const schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     users: [User!]!
     user(id: ID): User
@@ -52,29 +49,25 @@ const schema = buildSchema(`
     body: String!
     createdAt: String
   }
-`);
+`;
 
-const rootValue = {
-  users: () => db.users.map(user => new User(user)),
-  user: args => db.users.find(user => user.id === args.id),
-  messages: () => db.messages,
-  addUser: ({email, name}) => {
-    const user = {
-      id: crypto.randomBytes(10).toString('hex'),
-      email,
-      name
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: (root, {id}) => db.users.find(user => user.id === args.id),
+    messages: () => db.messages,
+  },
+  Mutattion: {
+    addUser: (root, {email, name}) => {
+      const user = {
+        id: crypto.randomBytes(10).toString('hex'),
+        email,
+        name
+      }
+  
+      db.users.push(user)
+  
+      return user
     }
-
-    db.users.push(user)
-
-    return user
   }
 }
-
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue,
-  graphiql: true
-}));
-
-app.listen(3000, () => console.log('listening on port 3000'));
