1c56166a6849e91066308d916474842d009fd9e9
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 17:41:25 -0800

    Completed app

--- ReactToDo/client/src/components/TodoItem.js
+++ ReactToDo/client/src/components/TodoItem.js
@@ -29,7 +29,9 @@ export class TodoItem extends Component {
 
 // PropTypes
 TodoItem.propTypes = {
-  todos: PropTypes.array.isRequired
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 
 const btnStyle = {
