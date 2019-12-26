af1eb85b12cb7177fa961fa32604c75e98fcb80f
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 23:41:17 -0800

    Set up port

--- GMERN/server/app.js
+++ GMERN/server/app.js
@@ -1,4 +1,38 @@
 'use strict';
 const express = require('express');
-const { graphql, buildSchema} = require('graphql');
 const app = express();
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+
+const db = {
+  users: [
+    {id: '1', email: 'alex@gmail.com', name: 'Alex'},
+    {id: '2', email: 'max@gmail.com', name: 'Max'},
+    {id: '3', email: 'steve@gmail.com', name: 'Steve'},
+  ]
+}
+
+const schema = buildSchema(`
+  type Query {
+    users: [User!]!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    name: String
+    avatarUrl: String
+  }
+`);
+
+const rootValue = {
+  users: () => db.users
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue,
+  graphiql: true
+}));
+
+app.listen(3000, () => console.log('listening on port 3000'));
