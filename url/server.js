4611c0cb344ee34f5476ddbfae9a01636db41a13
Estefan Hu <estefanhu074@gmail.com>
2020-04-26 00:30:03 -0700

    added database call

--- url/server.js
+++ url/server.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
+const ShortUrl = require('./models/shorturl');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/urlShortener', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(express.urlencoded({
+	extended: false
+}));
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-	res.render('index');
+app.get('/', async (req, res) => {
+	const shortUrl = await ShortUrl.find()
+	res.render('index', {
+		shortUrl: shortUrl
+	});
 });
 
-app.post('/shorUrls', (req, res) => {
+app.post('/shortUrls', async (req, res) => {
+	await ShortUrl.create({
+		full: req.body.fullUrl
+	});
 
+	res.redirect('/');
 });
 
 app.listen(process.env.PORT || 5000, () => {
-	console.log('Server running...')});
+	console.log('Server running...')
+});
