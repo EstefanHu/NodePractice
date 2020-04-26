1ea98813db0c5a6ee3ffff4a3d9612b995ebb277
Estefan Hu <estefanhu074@gmail.com>
2020-04-26 00:19:14 -0700

    added models

--- url/server.js
+++ url/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/urlShortener', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	res.render('index');
-})
+});
+
+app.post('/shorUrls', (req, res) => {
+
+});
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('Server running...')});
